0cd253a066a9af676b1d0fd426df21268bd70fde
David Nault <dnault@mac.com>
2015-05-27 19:14:35 -0700

    streams, yeah

--- src/main/java/com/github/dnault/bozbar/StandardBozbarScanner.java
+++ src/main/java/com/github/dnault/bozbar/StandardBozbarScanner.java
@@ -1,12 +1,11 @@
 package com.github.dnault.bozbar;
 
-import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
 import static org.springframework.util.ClassUtils.getAllInterfaces;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.github.dnault.bozbar.annotation.Remotable;
 import com.github.dnault.bozbar.internal.BozbarMethod;
@@ -14,22 +13,14 @@ import com.github.dnault.bozbar.internal.BozbarMethod;
 public class StandardBozbarScanner implements BozbarScanner {
     @Override
     public Collection<BozbarMethod> scan(Object o) {
-        List<BozbarMethod> methods = new ArrayList<>();
-
-        for (Class<?> iface : getAllInterfaces(o)) {
-            Remotable annotation = iface.getAnnotation(Remotable.class);
-            if (annotation != null) {
-                methods.addAll(scan(o, iface, annotation.value()));
-            }
-        }
-
-
-        return methods;
+        return Arrays.stream(getAllInterfaces(o))
+                .filter(iface -> iface.isAnnotationPresent(Remotable.class))
+                .flatMap(iface -> scan(o, iface, iface.getAnnotation(Remotable.class).value()))
+                .collect(toList());
     }
 
-    protected Collection<BozbarMethod> scan(Object owner, Class<?> iface, String namespace) {
-        return stream(iface.getMethods())
-                .map(method -> new BozbarMethod(namespace, owner, method, iface))
-                .collect(Collectors.toList());
+    protected Stream<BozbarMethod> scan(Object owner, Class<?> iface, String namespace) {
+        return Arrays.stream(iface.getMethods())
+                .map(method -> new BozbarMethod(namespace, owner, method, iface));
     }
 }
