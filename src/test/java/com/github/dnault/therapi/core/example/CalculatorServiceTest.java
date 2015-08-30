26002213fb9aca78d10cb5aa7b8764a407727fc8
David Nault <dnault@mac.com>
2015-08-29 23:13:33 -0700

    clean up example service test

--- src/test/java/com/github/dnault/therapi/core/example/CalculatorServiceTest.java
+++ src/test/java/com/github/dnault/therapi/core/example/CalculatorServiceTest.java
@@ -1,26 +1,18 @@
 package com.github.dnault.therapi.core.example;
 
-import static org.junit.Assert.assertEquals;
-
-import com.github.dnault.therapi.core.MethodRegistry;
-import com.github.dnault.therapi.core.internal.JacksonHelper;
+import com.github.dnault.therapi.core.AbstractMethodRegistryTest;
 import com.github.dnault.therapi.example.CalculatorServiceImpl;
+import org.junit.Before;
 import org.junit.Test;
 
-public class CalculatorServiceTest {
-    MethodRegistry context = new MethodRegistry(JacksonHelper.newLenientObjectMapper());
-
-    {
-        context.scan(new CalculatorServiceImpl());
+public class CalculatorServiceTest extends AbstractMethodRegistryTest {
+    @Before
+    public void setup() {
+        registry.scan(new CalculatorServiceImpl());
     }
-
 
     @Test
     public void multiplyFractions() throws Exception {
         check("calculator.multiplyFractions", "[[{numerator:2, denominator:3},{numerator:4, denominator:5}]]", "{numerator:8, denominator:15}");
-    }
-
-    protected void check(String methodName, String args, String expectedResult) throws Exception {
-        assertEquals(context.getObjectMapper().readTree(expectedResult), context.invoke(methodName, context.getObjectMapper().readTree(args)));
     }
 }
