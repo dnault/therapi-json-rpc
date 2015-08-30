d705742957b851ff41785804fd36b2dba2169f46
David Nault <dnault@mac.com>
2015-08-29 17:35:49 -0700

    whitespace cleanup

--- src/test/java/com/github/dnault/therapi/jsonrpc/JsonRpcDispatcherImplTest.java
+++ src/test/java/com/github/dnault/therapi/jsonrpc/JsonRpcDispatcherImplTest.java
@@ -135,28 +135,28 @@ public class JsonRpcDispatcherImplTest {
     @Test
     public void rpc_call_batch() throws Exception {
         check("[\n" +
-                        "        {'jsonrpc': '2.0', 'method': 'sum', 'params': [1,2,4], 'id': '1'},\n" +
-                        "        {'jsonrpc': '2.0', 'method': 'notify_hello', 'params': [7]},\n" +
-                        "        {'jsonrpc': '2.0', 'method': 'subtract', 'params': [42,23], 'id': '2'},\n" +
-                        "        {'foo': 'boo'},\n" +
-                        "        {'jsonrpc': '2.0', 'method': 'foo.get', 'params': {'name': 'myself'}, 'id': '5'},\n" +
-                        "        {'jsonrpc': '2.0', 'method': 'get_data', 'id': '9'} \n" +
-                        "    ]",
+                        "  {'jsonrpc': '2.0', 'method': 'sum', 'params': [1,2,4], 'id': '1'},\n" +
+                        "  {'jsonrpc': '2.0', 'method': 'notify_hello', 'params': [7]},\n" +
+                        "  {'jsonrpc': '2.0', 'method': 'subtract', 'params': [42,23], 'id': '2'},\n" +
+                        "  {'foo': 'boo'},\n" +
+                        "  {'jsonrpc': '2.0', 'method': 'foo.get', 'params': {'name': 'myself'}, 'id': '5'},\n" +
+                        "  {'jsonrpc': '2.0', 'method': 'get_data', 'id': '9'} \n" +
+                        "]",
                 "[\n" +
-                        "        {'jsonrpc': '2.0', 'result': 7, 'id': '1'},\n" +
-                        "        {'jsonrpc': '2.0', 'result': 19, 'id': '2'},\n" +
-                        "        {'jsonrpc': '2.0', 'error': {'code': -32600, 'message': 'Invalid request'}, 'id': null},\n" +
-                        "        {'jsonrpc': '2.0', 'error': {'code': -32601, 'message': 'Method \\'foo.get\\' not found'}, 'id': '5'},\n" +
-                        "        {'jsonrpc': '2.0', 'result': ['hello', 5], 'id': '9'}\n" +
-                        "    ]");
+                        "  {'jsonrpc': '2.0', 'result': 7, 'id': '1'},\n" +
+                        "  {'jsonrpc': '2.0', 'result': 19, 'id': '2'},\n" +
+                        "  {'jsonrpc': '2.0', 'error': {'code': -32600, 'message': 'Invalid request'}, 'id': null},\n" +
+                        "  {'jsonrpc': '2.0', 'error': {'code': -32601, 'message': 'Method \\'foo.get\\' not found'}, 'id': '5'},\n" +
+                        "  {'jsonrpc': '2.0', 'result': ['hello', 5], 'id': '9'}\n" +
+                        "]");
     }
 
     @Test
     public void rpc_call_batch_all_notifications() throws Exception {
         check("[\n" +
-                        "        {'jsonrpc': '2.0', 'method': 'notify_sum', 'params': [1,2,4]},\n" +
-                        "        {'jsonrpc': '2.0', 'method': 'notify_hello', 'params': [7]}\n" +
-                        "    ]",
+                        "  {'jsonrpc': '2.0', 'method': 'notify_sum', 'params': [1,2,4]},\n" +
+                        "  {'jsonrpc': '2.0', 'method': 'notify_hello', 'params': [7]}\n" +
+                        "]",
                 "");
     }
 
