800c37ad7a15c4979bad89c7572a56f6e96d9f66
David Nault <dnault@mac.com>
2016-03-17 17:19:26 -0700

    generate schemas for method invocations, for use with json-forms

--- src/main/java/com/github/therapi/apidoc/JsonSchemaProvider.java
+++ src/main/java/com/github/therapi/apidoc/JsonSchemaProvider.java
@@ -1,16 +1,52 @@
 package com.github.therapi.apidoc;
 
+import static com.google.common.base.Throwables.propagate;
+
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Optional;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
 import com.fasterxml.jackson.module.jsonSchema.factories.SchemaFactoryWrapper;
 import com.fasterxml.jackson.module.jsonSchema.factories.VisitorContext;
+import com.github.therapi.core.internal.MethodDefinition;
+import com.github.therapi.core.internal.ParameterDefinition;
 
 public class JsonSchemaProvider {
+
+    public String getSchema(ObjectMapper objectMapper, MethodDefinition methodDef) {
+        try {
+            ObjectNode methodNode = objectMapper.createObjectNode();
+            methodNode.put("type", "object");
+            methodNode.put("id", "urn:jsonschema:com:github:therapi:method:" + methodDef.getQualifiedName("."));
+
+            Map<String, JsonNode> properties = new LinkedHashMap<>();
+            methodNode.putPOJO("properties", properties);
+
+            for (ParameterDefinition paramDef : methodDef.getParameters()) {
+                properties.put(paramDef.getName(), getParamSchema(objectMapper, paramDef));
+            }
+
+            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(methodNode);
+
+        } catch (JsonProcessingException e) {
+            throw propagate(e);
+        }
+    }
+
+    private JsonNode getParamSchema(ObjectMapper objectMapper, ParameterDefinition paramDef) throws JsonMappingException {
+        SchemaFactoryWrapper visitor = new SchemaFactoryWrapper();
+        objectMapper.acceptJsonFormatVisitor(objectMapper.constructType(paramDef.getType().getType()), visitor);
+        JsonSchema jsonSchema = visitor.finalSchema();
+        return objectMapper.convertValue(jsonSchema, JsonNode.class);
+    }
 
     public Optional<String> getSchema(ObjectMapper objectMapper, Class modelClass) throws IOException {
         SchemaFactoryWrapper visitor = new SchemaFactoryWrapper();
--- src/test/java/com/github/therapi/apidoc/JsonSchemaProviderTest.java
+++ src/test/java/com/github/therapi/apidoc/JsonSchemaProviderTest.java
@@ -1,27 +1,149 @@
 package com.github.therapi.apidoc;
 
+import static com.github.therapi.core.internal.JacksonHelper.newLenientObjectMapper;
 import static net.javacrumbs.jsonunit.JsonAssert.assertJsonEquals;
 import static org.junit.Assert.assertFalse;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.therapi.core.StandardParameterIntrospector;
+import com.github.therapi.core.internal.MethodDefinition;
+import com.github.therapi.core.internal.ParameterDefinition;
 import org.junit.Test;
 
 public class JsonSchemaProviderTest {
 
+    private enum Color {
+        RED, GREEN, BLUE
+    }
+
     private interface Zoo {
         String getName();
+
         List<Animal> getAnimals();
     }
 
     private interface Animal {
         String getSpecies();
+
+        Color getColor();
     }
 
     private static class NoPublicFields {
         private int x;
+    }
+
+    @SuppressWarnings("unused")
+    public void exampleMethod(String string,
+                              boolean primitiveBoolean,
+                              Boolean boxedBoolean,
+                              int primitiveInt,
+                              Integer BoxedInt,
+                              long primitiveLong,
+                              Long boxedLong,
+                              float primitiveFloat,
+                              Float boxedFloat,
+                              double primitiveDouble,
+                              Double boxedDouble,
+                              String[] stringArray,
+                              List<String> stringList,
+                              Zoo zoo) {
+    }
+
+    @Test
+    public void testGetMethodSchema() throws Exception {
+        ObjectMapper objectMapper = newLenientObjectMapper();
+
+        Method method = Arrays.asList(getClass().getMethods()).stream()
+                .filter(m -> m.getName().equals("exampleMethod"))
+                .findAny().get();
+
+        List<ParameterDefinition> paramDefs = new StandardParameterIntrospector(objectMapper).findParameters(method, this);
+        MethodDefinition methodDef = new MethodDefinition("test", "exampleMethod", method, this, paramDefs);
+        String result = new JsonSchemaProvider().getSchema(objectMapper, methodDef);
+
+        String expected = "{\n" +
+                "  \"type\" : \"object\",\n" +
+                "  \"id\" : \"urn:jsonschema:com:github:therapi:method:test.exampleMethod\",\n" +
+                "  \"properties\" : {\n" +
+                "    \"string\" : {\n" +
+                "      \"type\" : \"string\"\n" +
+                "    },\n" +
+                "    \"primitiveBoolean\" : {\n" +
+                "      \"type\" : \"boolean\"\n" +
+                "    },\n" +
+                "    \"boxedBoolean\" : {\n" +
+                "      \"type\" : \"boolean\"\n" +
+                "    },\n" +
+                "    \"primitiveInt\" : {\n" +
+                "      \"type\" : \"integer\"\n" +
+                "    },\n" +
+                "    \"BoxedInt\" : {\n" +
+                "      \"type\" : \"integer\"\n" +
+                "    },\n" +
+                "    \"primitiveLong\" : {\n" +
+                "      \"type\" : \"integer\"\n" +
+                "    },\n" +
+                "    \"boxedLong\" : {\n" +
+                "      \"type\" : \"integer\"\n" +
+                "    },\n" +
+                "    \"primitiveFloat\" : {\n" +
+                "      \"type\" : \"number\"\n" +
+                "    },\n" +
+                "    \"boxedFloat\" : {\n" +
+                "      \"type\" : \"number\"\n" +
+                "    },\n" +
+                "    \"primitiveDouble\" : {\n" +
+                "      \"type\" : \"number\"\n" +
+                "    },\n" +
+                "    \"boxedDouble\" : {\n" +
+                "      \"type\" : \"number\"\n" +
+                "    },\n" +
+                "    \"stringArray\" : {\n" +
+                "      \"type\" : \"array\",\n" +
+                "      \"items\" : {\n" +
+                "        \"type\" : \"string\"\n" +
+                "      }\n" +
+                "    },\n" +
+                "    \"stringList\" : {\n" +
+                "      \"type\" : \"array\",\n" +
+                "      \"items\" : {\n" +
+                "        \"type\" : \"string\"\n" +
+                "      }\n" +
+                "    },\n" +
+                "    \"zoo\" : {\n" +
+                "      \"type\" : \"object\",\n" +
+                "      \"id\" : \"urn:jsonschema:com:github:therapi:apidoc:JsonSchemaProviderTest:Zoo\",\n" +
+                "      \"properties\" : {\n" +
+                "        \"name\" : {\n" +
+                "          \"type\" : \"string\"\n" +
+                "        },\n" +
+                "        \"animals\" : {\n" +
+                "          \"type\" : \"array\",\n" +
+                "          \"items\" : {\n" +
+                "            \"type\" : \"object\",\n" +
+                "            \"id\" : \"urn:jsonschema:com:github:therapi:apidoc:JsonSchemaProviderTest:Animal\",\n" +
+                "            \"properties\" : {\n" +
+                "              \"color\" : {\n" +
+                "                \"type\" : \"string\",\n" +
+                "                \"enum\" : [ \"RED\", \"GREEN\", \"BLUE\" ]\n" +
+                "              },\n" +
+                "              \"species\" : {\n" +
+                "                \"type\" : \"string\"\n" +
+                "              }\n" +
+                "            }\n" +
+                "          }\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}\n";
+
+        assertJsonEquals(expected, result);
     }
 
     @Test
