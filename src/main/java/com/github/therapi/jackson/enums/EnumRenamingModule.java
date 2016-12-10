9f790d520da8bca97e2dbfb34f5f60f7536228b6
David Nault <dnault@mac.com>
2016-12-09 20:35:05 -0800

    update for Jackson 2.7+

--- src/main/java/com/github/therapi/jackson/enums/EnumRenamingModule.java
+++ src/main/java/com/github/therapi/jackson/enums/EnumRenamingModule.java
@@ -8,8 +8,19 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
  * Customizes the way Jackson serializes enums.
  */
 public abstract class EnumRenamingModule extends SimpleModule {
+    private boolean overrideExistingNames;
+
     public EnumRenamingModule() {
         super("therapi-enum-renaming");
+    }
+
+    /**
+     * Configures the module to clobber any enum names set by
+     * a previous annotation introspector.
+     */
+    public EnumRenamingModule overrideExistingNames() {
+        this.overrideExistingNames = true;
+        return this;
     }
 
     @Override
@@ -19,9 +30,13 @@ public abstract class EnumRenamingModule extends SimpleModule {
     }
 
     private class EnumNamingAnnotationIntrospector extends NopAnnotationIntrospector {
-        @Override
-        public String findEnumValue(Enum<?> value) {
-            return EnumRenamingModule.this.getName(value);
+        public String[] findEnumValues(Class<?> enumType, Enum<?>[] enumValues, String[] names) {
+            for (int i = 0; i < enumValues.length; i++) {
+                if (names[i] == null || overrideExistingNames) {
+                    names[i] = EnumRenamingModule.this.getName(enumValues[i]);
+                }
+            }
+            return names;
         }
     }
 
