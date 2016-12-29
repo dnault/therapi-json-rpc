023d20b1d55c740a3dae369a763b29190b60046e
David Nault <dnault@mac.com>
2016-12-29 10:21:35 -0800

    Add an optional component for configuring Spring WebMVC to serve the static resources required by the API documentation page.
    Upgrade spring-boot to 1.4.3

---
+++ src/main/java/com/github/therapi/jsonrpc/web/ApidocWebMvcConfigurer.java
@@ -0,0 +1,21 @@
+package com.github.therapi.jsonrpc.web;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Configures Spring to serve the static resources required by the API documentation web page.
+ * Only necessary if the API documentation page renders incorrectly without it.
+ * <p>
+ * Normally the static resources are automatically served by the servlet container,
+ * but Spring's default Web MVC configuration may prevent that from happening.
+ * <p>
+ * To activate, add a bean of this class to your application context.
+ */
+public class ApidocWebMvcConfigurer extends WebMvcConfigurerAdapter {
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/therapi/**")
+        .addResourceLocations("classpath:/META-INF/resources/therapi/");
+  }
+}
