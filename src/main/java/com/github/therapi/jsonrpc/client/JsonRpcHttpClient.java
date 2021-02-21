71651e18f13481d073d381784105087cfb062cdc
David Nault <dnault@mac.com>
2021-02-21 11:44:58 -0800

    breaking changes, so let's bump to 0.5.0

--- src/main/java/com/github/therapi/jsonrpc/client/JsonRpcHttpClient.java
+++
@@ -1,8 +0,0 @@
-package com.github.therapi.jsonrpc.client;
-
-/**
- * @deprecated in favor of {@link JsonRpcTransport}
- */
-@Deprecated
-public interface JsonRpcHttpClient extends JsonRpcTransport {
-}
